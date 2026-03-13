6f9f1fc16590069e31e9b711c9d6d37874d99245
Jose Valverde <106929528+JoseValverdeS@users.noreply.github.com>
2026-03-13 10:21:26 -0600

    Init spring boot

---
+++ src/main/java/com/atara/deb/ataraapi/AtaraApiApplication.java
@@ -0,0 +1,13 @@
+package com.atara.deb.ataraapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaraApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaraApiApplication.class, args);
+	}
+
+}
